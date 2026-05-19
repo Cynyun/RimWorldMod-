0eb834eeb3a2838ef4e30726016009c1104524ed
RimWorldModManager <rimworld@modmanager.local>
2026-05-19 17:38:30 +0800

    Fix: Remove WPF-UI resources that don't exist

---
+++ Converters/NullImageConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace RimWorldModManager.Converters
+{
+    public class NullImageConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length > 0 && values[0] is string path && !string.IsNullOrEmpty(path) && File.Exists(path))
+            {
+                try
+                {
+                    return new BitmapImage(new Uri(path));
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
